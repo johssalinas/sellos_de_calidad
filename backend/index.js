f915197cc80d91314ea231d84521cf275edc9af0
Johs Salinas <brayansalinas2345@gmail.com>
2023-03-24 00:40:20 -0500

    AJUSTE - Se cambia el sistema para que haya una lista predeterminada de opciones y al seleccionarlas no vuelven a aparecer

--- backend/index.js
+++ backend/index.js
@@ -8,113 +8,113 @@ app.use(cors());
 
 // Configurar la conexión a la base de datos
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_marval",
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "db_marval",
 });
 
 // Conectar a la base de datos
 connection.connect((err) => {
-  if (err) {
-    console.error("Error al conectar a la base de datos: ", err);
-    return;
-  }
-  console.log("Conectado a la base de datos");
+	if (err) {
+		console.error("Error al conectar a la base de datos: ", err);
+		return;
+	}
+	console.log("Conectado a la base de datos");
 });
 
 // Definir las rutas
 app.get("/proyectos", (req, res) => {
-  const query = "SELECT * FROM proyecto";
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error("Error al obtener los proyectos: ", err);
-      res.status(500).send("Error al obtener los proyectos");
-      return;
-    }
-    res.json(results);
-  });
+	const query = "SELECT * FROM proyecto";
+	connection.query(query, (err, results) => {
+		if (err) {
+			console.error("Error al obtener los proyectos: ", err);
+			res.status(500).send("Error al obtener los proyectos");
+			return;
+		}
+		res.json(results);
+	});
 });
 
-app.get("/proyectos/:id/agrupaciones", (req, res) => {
-  const { id } = req.params;
-  const query = `SELECT * FROM etapaproyecto WHERE id_proyecto = ${id}`;
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error("Error al obtener las agrupaciones: ", err);
-      res.status(500).send("Error al obtener las agrupaciones");
-      return;
-    }
-    res.json(results);
-  });
+app.get("/agrupaciones", (req, res) => {
+	const query = "SELECT * FROM etapaproyecto";
+	connection.query(query, (err, results) => {
+		if (err) {
+			console.error("Error al obtener las agrupaciones: ", err);
+			res.status(500).send("Error al obtener las agrupaciones");
+			return;
+		}
+		res.json(results);
+	});
 });
 
 app.get("/sellos-calidad", (req, res) => {
-  const query = "SELECT * FROM sellocalidad";
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error("Error al obtener los sellos de calidad: ", err);
-      res.status(500).send("Error al obtener los sellos de calidad");
-      return;
-    }
-    res.json(results);
-  });
+	const query = "SELECT * FROM sellocalidad";
+	connection.query(query, (err, results) => {
+		if (err) {
+			console.error("Error al obtener los sellos de calidad: ", err);
+			res.status(500).send("Error al obtener los sellos de calidad");
+			return;
+		}
+		res.json(results);
+	});
 });
 
 app.get("/contratistas", (req, res) => {
-  const query = "SELECT * FROM contratista";
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error("Error al obtener los contratistas: ", err);
-      res.status(500).send("Error al obtener los contratistas");
-      return;
-    }
-    res.json(results);
-  });
+	const query = "SELECT * FROM contratista";
+	connection.query(query, (err, results) => {
+		if (err) {
+			console.error("Error al obtener los contratistas: ", err);
+			res.status(500).send("Error al obtener los contratistas");
+			return;
+		}
+		res.json(results);
+	});
 });
 
 app.get(
-  "/unidades-disponibles/:id_proyecto/:id_etapa/:id_sello/:id_contratista",
-  (req, res) => {
-    const { id_proyecto, id_etapa, id_sello, id_contratista } = req.params;
-    const query = `SELECT * FROM unidadestomasello WHERE 
-                          id_proyecto = ${id_proyecto} AND 
-                          id_etapa = ${id_etapa} AND 
-                          id_sello = ${id_sello} AND 
-                          id_contratista = ${id_contratista} AND 
-                          seleccionada = 0`;
-    connection.query(query, (err, results) => {
-      if (err) {
-        console.error("Error al obtener las unidades disponibles: ", err);
-        res.status(500).send("Error al obtener las unidades disponibles");
-        return;
-      }
-      res.json(results);
-    });
-  }
+	"/unidades-disponibles",
+	(req, res) => {
+		const { id_proyecto, id_etapa, id_sello, id_contratista } = req.params;
+		const query = `SELECT * FROM unidadessellodisponibles WHERE 
+                          estado = 0`;
+		connection.query(query, (err, results) => {
+			if (err) {
+				console.error("Error al obtener las unidades disponibles: ", err);
+				res.status(500).send("Error al obtener las unidades disponibles");
+				return;
+			}
+			res.json(results);
+		});
+	},
 );
 
 app.post("/tomas-sello", (req, res) => {
-  const { id_unidad, id_proyecto, id_etapa, id_sello, id_contratista } =
-    req.body;
-  //actuaizar unidades seleccionadas
-  const query = `UPDATE unidadestomasello SET seleccionada = 1 WHERE 
-                          id_unidad = ${id_unidad} AND 
-                          id_proyecto = ${id_proyecto} AND
-                          id_etapa = ${id_etapa} AND 
-                          id_sello = ${id_sello} AND 
-                          id_contratista = ${id_contratista}`;
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Error al registrar toma de sello");
-    } else {
-      console.log("Toma de sello registrada exitosamente");
-      res.status(200).send("Toma de sello registrada exitosamente");
-    }
+	const { id_unidadesdisponibles, id_proyecto, id_etapa, id_sello, id_contratista } =
+		req.body;
+	//insertar unidades seleccionadas
+  const query = `INSERT INTO unidadestomasello (id_unidadesdisponibles , id_proyecto, id_etapa, id_sello, id_contratista) VALUES (${id_unidadesdisponibles}, ${id_proyecto}, ${id_etapa}, ${id_sello}, ${id_contratista})`;
+	connection.query(query, (err, results) => {
+		if (err) {
+			console.log(err);
+			res.status(500).send("Error al registrar toma de sello");
+		} else {
+			console.log("Toma de sello registrada exitosamente");
+			res.status(200).send("Toma de sello registrada exitosamente");
+		}
+	});
+});
+
+// Ruta para actualizar un registro en la tabla "unidadessellodisponibles"
+app.get('/actualizar-unidad/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = `UPDATE unidadessellodisponibles SET estado = 1 WHERE id_unidadesdisponibles = ${id}`;
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(`Registro con id ${id} actualizado correctamente en la tabla unidadessellodisponibles!`);
   });
 });
 
 app.listen(3001, () => {
-  console.log("Servidor iniciado en el puerto 3001");
+	console.log("Servidor iniciado en el puerto 3001");
 });
--- frontend/sellos_calidad/src/componentes/sellos_calidad.js
+++ frontend/sellos_calidad/src/componentes/sellos_calidad.js
@@ -3,280 +3,279 @@ import axios from "axios";
 import "./css/sellos_calidad.css";
 
 const ProjectSelection = () => {
-  const [projects, setProjects] = useState([]);
-  const [agroups, setAgroups] = useState([]);
-  const [sellos, setSellos] = useState([]);
-  const [contratistas, setContratistas] = useState([]);
-  const [unidadesSellos, setUnidadesSellos] = useState([]);
-  
-  const [selectedProject, setSelectedProject] = useState(null);
-  const [selectedAgroup, setSelectedAgroup] = useState(null);
-  const [selectedSellos, setSelectedSellos] = useState(null);
-  const [selectedContratistas, setSelectedContratistas] = useState(null);
-  const [confirmationMessage, setConfirmationMessage] = useState("");
-  const [selectedUnidadesSellos, setSelectedUnidadesSellos] = useState({
-    unidadesSellos: [],
-  });
-  const [loading, setLoading] = useState(true);
+	const [projects, setProjects] = useState([]);
+	const [agroups, setAgroups] = useState([]);
+	const [sellos, setSellos] = useState([]);
+	const [contratistas, setContratistas] = useState([]);
+	const [unidadesSellos, setUnidadesSellos] = useState([]);
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      setLoading(true);
-      try {
-        const res = await axios.get("http://localhost:3001/proyectos");
-        setProjects(res.data);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchProjects();
-  }, []);
+	const [selectedProject, setSelectedProject] = useState(null);
+	const [selectedAgroup, setSelectedAgroup] = useState(null);
+	const [selectedSellos, setSelectedSellos] = useState(null);
+	const [selectedContratistas, setSelectedContratistas] = useState(null);
+	const [confirmationMessage, setConfirmationMessage] = useState("");
+	const [selectedUnidadesSellos, setSelectedUnidadesSellos] = useState({
+		unidadesSellos: [],
+	});
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchAgroups = async () => {
-      setLoading(true);
-      try {
-        const res = await axios.get(
-          `http://localhost:3001/proyectos/${selectedProject}/agrupaciones`
-        );
-        setAgroups(res.data);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    if (selectedProject) {
-      fetchAgroups();
-    }
-  }, [selectedProject]);
+	useEffect(() => {
+		const fetchProjects = async () => {
+			setLoading(true);
+			try {
+				const res = await axios.get("http://localhost:3001/proyectos");
+				setProjects(res.data);
+			} catch (err) {
+				console.log(err);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchProjects();
+	}, []);
 
-  useEffect(() => {
-    const fetchSellos = async () => {
-      setLoading(true);
-      try {
-        const res = await axios.get("http://localhost:3001/sellos-calidad");
-        setSellos(res.data);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchSellos();
-  }, []);
+	useEffect(() => {
+		const fetchAgroups = async () => {
+			setLoading(true);
+			try {
+				const res = await axios.get("http://localhost:3001/agrupaciones");
+				setAgroups(res.data);
+			} catch (err) {
+				console.log(err);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchAgroups();
+	}, []);
 
-  useEffect(() => {
-    const fetchContratistas = async () => {
-      setLoading(true);
-      try {
-        const res = await axios.get("http://localhost:3001/contratistas");
-        setContratistas(res.data);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchContratistas();
-  }, []);
+	useEffect(() => {
+		const fetchSellos = async () => {
+			setLoading(true);
+			try {
+				const res = await axios.get("http://localhost:3001/sellos-calidad");
+				setSellos(res.data);
+			} catch (err) {
+				console.log(err);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchSellos();
+	}, []);
 
-  useEffect(() => {
-    const fetchUnidadesSellos = async () => {
-      setLoading(true);
-      try {
-        const res = await axios.get(
-          `http://localhost:3001/unidades-disponibles/${selectedProject}/${selectedAgroup}/${selectedSellos}/${selectedContratistas}`
-        );
-        setUnidadesSellos(res.data);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    if (
-      selectedProject &&
-      selectedAgroup &&
-      selectedSellos &&
-      selectedContratistas
-      ) {
-        fetchUnidadesSellos();
-      }
-    }, [selectedProject, selectedAgroup, selectedSellos, selectedContratistas]);
-  
-    const handleProjectSelection = (e) => {
-      const projectId = e.target.value;
-      setSelectedProject(projectId);
-    };
-  
-    const handleAgroupSelection = (e) => {
-      const agroupId = e.target.value;
-      setSelectedAgroup(agroupId);
-    };
-  
-    const handleSellosSelection = (e) => {
-      const sellosId = e.target.value;
-      setSelectedSellos(sellosId);
-    };
-  
-    const handleContratistasSelection = (e) => {
-      const contratistasId = e.target.value;
-      setSelectedContratistas(contratistasId);
-    };
-  
-    const handleUnidadesSellosSelection = (e) => {
-      const unidadSelloId = e.target.value;
-      const isChecked = e.target.checked;
-      setSelectedUnidadesSellos((prevValues) => {
-        let unidadesSellos = [...prevValues.unidadesSellos];
-        if (isChecked) {
-          unidadesSellos.push(unidadSelloId);
-        } else {
-          unidadesSellos = unidadesSellos.filter((id)=> id !== unidadSelloId);
-        }
-        return { ...prevValues, unidadesSellos: unidadesSellos };
-      });
-    };
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      if (
-        selectedProject &&
-        selectedAgroup &&
-        selectedSellos &&
-        selectedContratistas &&
-        selectedUnidadesSellos.unidadesSellos.length > 0
-      ) {
-        try {
-          await Promise.all(
-            selectedUnidadesSellos.unidadesSellos.map((unidadSello) =>
-              axios.post("http://localhost:3001/tomas-sello", {
-                id_unidad: unidadSello,
-                id_proyecto: selectedProject,
-                id_etapa: selectedAgroup,
-                id_sello: selectedSellos,
-                id_contratista: selectedContratistas,
-              })
-            )
-          );
-          setConfirmationMessage("¡Los datos han sido enviados correctamente!");
-        } catch (err) {
-          console.log(err);
-        }
-      } else {
-        setConfirmationMessage("¡Todos los campos son requeridos!");
-      }
-    };
-  
-    return (
-      <div className="principal">
-        <div className="container">
-          <div className="titulo">
-            <h1>Toma de Sellos Calidad-Pisos</h1>
-          </div>
-          <form onSubmit={handleSubmit} className="select">
-            {loading ? (
-              <p>Cargando proyectos...</p>
-            ) : (
-              <select
-                onChange={handleProjectSelection}
-                value={selectedProject || "default"}
-                className="grid-2"
-              >
-                <option value="default">Proyecto</option>
-                {projects.map((project) => (
-                  <option
-                    key={project.id_proyecto}
-                    value={project.id_proyecto}
-                  >
-                    {project.nombre_proyecto}
-                  </option>
-                ))}
-              </select>
-            )}
-            {loading ? (
-              <p>Cargando agrupaciones...</p>
-            ) : (
-              <select
-                onChange={handleAgroupSelection}
-                value={selectedAgroup || "default"}
-                disabled={!selectedProject}
-              >
-                <option value="default">Agrupación</option>
-              {agroups.map((agroup) => (
-                <option key={agroup.id_etapa} value={agroup.id_etapa}>
-                  {agroup.nombre_etapa}
-                </option>
-              ))}
-            </select>
-          )}
-          {loading ? (
-            <p>Cargando sellos...</p>
-          ) : (
-            <select
-              onChange={handleSellosSelection}
-              value={selectedSellos || "default"}
-              disabled={!selectedProject}
-            >
-              <option value="default">Sello</option>
-              {sellos.map((sello) => (
-                <option key={sello.id_sello} value={sello.id_sello}>
-                  {sello.nombre_sello}
-                </option>
-              ))}
-            </select>
-          )}
-          {loading ? (
-            <p>Cargando contratistas...</p>
-          ) : (
-            <select
-              onChange={handleContratistasSelection}
-              value={selectedContratistas || "default"}
-              disabled={!selectedProject}
-              className="grid-2"
-            >
-              <option value="default">Contratista</option>
-              {contratistas.map((contratista) => (
-                <option
-                  key={contratista.id_contratista}
-                  value={contratista.id_contratista}
-                >
-                  {contratista.descContratista}
-                </option>
-              ))}
-            </select>
-          )}
-          {loading ? (
-            <p>Cargando unidades de sellos...</p>
-          ) : (
-            <div className="grid-2">
-              {unidadesSellos.map((unidadSello) => (
-                <div key={unidadSello.id_unidad} className="checkbox">
-                  <input
-                    type="checkbox"
-                    id={unidadSello.id_unidad}
-                    name={unidadSello.id_unidad}
-                    value={unidadSello.id_unidad}
-                    onChange={handleUnidadesSellosSelection}
-                  />
-                  <label htmlFor={unidadSello.id_unidad}>
-                    {unidadSello.nombre_unidad}
-                  </label>
-                </div>
-              ))}
-            </div>
-          )}
-          <button className="button grid-2" type="submit">
-            Guardar
-          </button>
-        </form>
-        {confirmationMessage && <p>{confirmationMessage}</p>}
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		const fetchContratistas = async () => {
+			setLoading(true);
+			try {
+				const res = await axios.get("http://localhost:3001/contratistas");
+				setContratistas(res.data);
+			} catch (err) {
+				console.log(err);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchContratistas();
+	}, []);
+
+	useEffect(() => {
+		const fetchUnidadesSellos = async () => {
+			setLoading(true);
+			try {
+				const res = await axios.get(
+					"http://localhost:3001/unidades-disponibles",
+				);
+				setUnidadesSellos(res.data);
+			} catch (err) {
+				console.log(err);
+			} finally {
+				setLoading(false);
+			}
+		};
+		if (
+			selectedProject &&
+			selectedAgroup &&
+			selectedSellos &&
+			selectedContratistas
+		) {
+			fetchUnidadesSellos();
+		}
+	}, [selectedProject, selectedAgroup, selectedSellos, selectedContratistas]);
+
+	const handleProjectSelection = (e) => {
+		const projectId = e.target.value;
+		setSelectedProject(projectId);
+	};
+
+	const handleAgroupSelection = (e) => {
+		const agroupId = e.target.value;
+		setSelectedAgroup(agroupId);
+	};
+
+	const handleSellosSelection = (e) => {
+		const sellosId = e.target.value;
+		setSelectedSellos(sellosId);
+	};
+
+	const handleContratistasSelection = (e) => {
+		const contratistasId = e.target.value;
+		setSelectedContratistas(contratistasId);
+	};
+
+	const handleUnidadesSellosSelection = (e) => {
+		const unidadSelloId = e.target.value;
+		const isChecked = e.target.checked;
+		setSelectedUnidadesSellos((prevValues) => {
+			let unidadesSellos = [...prevValues.unidadesSellos];
+			if (isChecked) {
+				unidadesSellos.push(unidadSelloId);
+			} else {
+				unidadesSellos = unidadesSellos.filter((id) => id !== unidadSelloId);
+			}
+			return { ...prevValues, unidadesSellos: unidadesSellos };
+		});
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		if (
+			selectedProject &&
+			selectedAgroup &&
+			selectedSellos &&
+			selectedContratistas &&
+			selectedUnidadesSellos.unidadesSellos.length > 0
+		) {
+			try {
+				await Promise.all(
+					selectedUnidadesSellos.unidadesSellos.map((unidadSello) =>
+						axios.post("http://localhost:3001/tomas-sello", {
+							id_unidadesdisponibles: unidadSello,
+							id_proyecto: selectedProject,
+							id_etapa: selectedAgroup,
+							id_sello: selectedSellos,
+							id_contratista: selectedContratistas,
+						}),
+					),
+				);
+				selectedUnidadesSellos.unidadesSellos.map((unidadSello) =>
+					axios.get(`http://localhost:3001/actualizar-unidad/${unidadSello}`),
+				);
+				setConfirmationMessage("¡Los datos han sido enviados correctamente!");
+			} catch (err) {
+				console.log(err);
+			}
+		} else {
+			setConfirmationMessage("¡Todos los campos son requeridos!");
+		}
+	};
+
+	return (
+		<div className="principal">
+			<div className="container">
+				<div className="titulo">
+					<h1>Toma de Sellos Calidad-Pisos</h1>
+				</div>
+				<form onSubmit={handleSubmit} className="select">
+					{loading ? (
+						<p>Cargando proyectos...</p>
+					) : (
+						<select
+							onChange={handleProjectSelection}
+							value={selectedProject || "default"}
+							className="grid-2"
+						>
+							<option value="default">Proyecto</option>
+							{projects.map((project) => (
+								<option key={project.id_proyecto} value={project.id_proyecto}>
+									{project.nombre_proyecto}
+								</option>
+							))}
+						</select>
+					)}
+					{loading ? (
+						<p>Cargando agrupaciones...</p>
+					) : (
+						<select
+							onChange={handleAgroupSelection}
+							value={selectedAgroup || "default"}
+							disabled={!selectedProject}
+						>
+							<option value="default">Agrupación</option>
+							{agroups.map((agroup) => (
+								<option key={agroup.id_etapa} value={agroup.id_etapa}>
+									{agroup.nombre_etapa}
+								</option>
+							))}
+						</select>
+					)}
+					{loading ? (
+						<p>Cargando sellos...</p>
+					) : (
+						<select
+							onChange={handleSellosSelection}
+							value={selectedSellos || "default"}
+							disabled={!selectedProject}
+						>
+							<option value="default">Sello</option>
+							{sellos.map((sello) => (
+								<option key={sello.id_sello} value={sello.id_sello}>
+									{sello.nombre_sello}
+								</option>
+							))}
+						</select>
+					)}
+					{loading ? (
+						<p>Cargando contratistas...</p>
+					) : (
+						<select
+							onChange={handleContratistasSelection}
+							value={selectedContratistas || "default"}
+							disabled={!selectedProject}
+							className="grid-2"
+						>
+							<option value="default">Contratista</option>
+							{contratistas.map((contratista) => (
+								<option
+									key={contratista.id_contratista}
+									value={contratista.id_contratista}
+								>
+									{contratista.descContratista}
+								</option>
+							))}
+						</select>
+					)}
+					{loading ? (
+						<p>Cargando unidades de sellos...</p>
+					) : (
+						<div className="grid-2">
+							{unidadesSellos.map((unidadSello) => (
+								<div
+									key={unidadSello.id_unidadesdisponibles}
+									className="checkbox"
+								>
+									<input
+										type="checkbox"
+										id={unidadSello.id_unidadesdisponibles}
+										name={unidadSello.id_unidadesdisponibles}
+										value={unidadSello.id_unidadesdisponibles}
+										onChange={handleUnidadesSellosSelection}
+									/>
+									<label htmlFor={unidadSello.id_unidadesdisponibles}>
+										{unidadSello.nombre_unidaddisponible}
+									</label>
+								</div>
+							))}
+						</div>
+					)}
+					<button className="button grid-2" type="submit">
+						Guardar
+					</button>
+				</form>
+				{confirmationMessage && <p>{confirmationMessage}</p>}
+			</div>
+		</div>
+	);
 };
 
 export default ProjectSelection;
